4fb9f8b8eaa10522cb64b10390b95e727d95a697
Juan Pablo Montoya <101369659+jmontoyavallejo@users.noreply.github.com>
2022-12-31 17:27:30 -0500

    first project end

--- clon/src/App.js
+++ clon/src/App.js
@@ -15,7 +15,7 @@ function App() {
           cargo='Ingeniero de Software'
           empresa='Amazon'
           testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
-          />
+        />
         <Testimonio
           nombre='Sarah Chima'
           pais='Nigeria'
@@ -23,7 +23,7 @@ function App() {
           cargo='Ingeniera de Software'
           empresa='ChatDesk'
           testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
-          />
+        />
         <Testimonio
           nombre='Emma Bostian'
           pais='Suecia'
